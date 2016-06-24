bc8c9a94722b28114f43f7d82b7008dd1c3c6948
jacquesnier <jacquesnier@hotmail.com>
2016-06-24 09:12:49 -0300

    Criar e implementar NoticiaRepository e NoticiaService

--- src/main/java/br/ufc/repository/NoticiaRepository.java
+++ src/main/java/br/ufc/repository/NoticiaRepository.java
@@ -5,11 +5,12 @@ import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import br.ufc.model.Comentario;
 import br.ufc.model.Secao;
 import br.ufc.model.Noticia;
 
 @Repository
 public interface NoticiaRepository extends CrudRepository<Noticia, Long> {
-	//public List<Noticia> findNoticiaBySecao(Secao secao);
-	//public List<Noticia> findNoticiaByNome(String nome);
+	public List<Noticia> findNoticiaBySecao(Secao secao);
+	public List<Noticia> findNoticiaByNome(String nome);
 }
